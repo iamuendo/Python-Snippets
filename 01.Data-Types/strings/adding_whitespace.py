4da18558212a6692e84c06abcca10fbab309c49c
iamuendo <iamuendo@gmail.com>
2022-06-07 10:38:15 +0300

    Update folders

---
+++ 01.Data-Types/strings/adding_whitespace.py
@@ -0,0 +1,6 @@
+# Whitespace - refers to any nonprinting character, such as spaces
+# \n - to print values on a new line 
+# \t - to add tabs
+
+languages = "These are programming languages:\n\t-Python\n\t-Java\n\t-PHP"
+print(languages)
---
+++ 01.Data-Types/strings/stripping_whitespace.py
@@ -0,0 +1,8 @@
+username = 'Pythonuser001    '
+bio = '   I love python programming'
+name = ' Isaac Muendo '
+
+# rstrip() - strips whitespaces on the right side of the string
+# lstrip() - strips whitespaces on the left side of the string
+# strip() - strips whitespaces from both the right & left side of the string
+print(username.rstrip(), bio.lstrip(), name.strip())
