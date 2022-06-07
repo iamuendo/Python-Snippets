51976703a3e3978844b0421ea7ea08b5d6fbd159
iamuendo <iamuendo@gmail.com>
2022-06-07 10:54:35 +0300

    Variables

---
+++ 02.Variables-and-Identifiers/variables.py
@@ -0,0 +1,6 @@
+# message is a variable name that has been assigned the value Hello, World!
+message = "Hello, World!"
+print(message)
+
+message = "Hello, World! This is python crash course"
+print(message)
