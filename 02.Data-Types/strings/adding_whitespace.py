02bf34a57bdf3ad50b8272b47658853366516fd6
Isaac Muendo <52902936+iamuendo@users.noreply.github.com>
2022-06-07 10:40:49 +0300

    Delete 02.Data-Types directory

--- 02.Data-Types/strings/adding_whitespace.py
+++
@@ -1,6 +0,0 @@
-# Whitespace - refers to any nonprinting character, such as spaces
-# \n - to print values on a new line 
-# \t - to add tabs
-
-languages = "These are programming languages:\n\t-Python\n\t-Java\n\t-PHP"
-print(languages)
--- 02.Data-Types/strings/combining_strings.py
+++
@@ -1,9 +0,0 @@
-# The method for combining strings is known as concatenation.
-first_name = "john"
-last_name = "doe"
-
-full_name = first_name + " " +last_name
-
-# Display full name
-message = "Hello, " + full_name.title() + "!"
-print(message)
